45bcf0437f30cb8705659dec2c8a866920545aee
Stephen Russell <stephenjohnrussell@me.com>
2017-09-28 13:39:00 +0100

    Expanding test coverage

--- route/Hill.go
+++ route/Hill.go
@@ -77,9 +77,6 @@ func (h *Hill) Category() string {
 func (r *Route) FindClimbs() {
 	locations := r.Data.Track.Segments.Locations
 	var sec Section
-	// Markers to help logic in sections:
-	// s = started
-	// e = finished
 	s := false
 	e := false
 	for i := 0; i < len(locations); i++ {
@@ -112,8 +109,6 @@ func (r *Route) FindClimbs() {
 		if r.Sections[i].Grade > 3 {
 			var hill Hill
 			hill.Start = r.Sections[i]
-			//log.Printf("Start Location of Hill: %v,%v grade: %v\n", hill.Start.Start.Latitude,
-			//	hill.Start.Start.Longitude, r.Sections[i].Grade)
 			hill.AverageGrade = hill.Start.Grade
 			for j := i; j < len(r.Sections); j++ {
 				if (hill.AverageGrade+r.Sections[j].Grade)/float64(len(hill.Sections)) < 3 {
@@ -127,25 +122,18 @@ func (r *Route) FindClimbs() {
 					}
 					if tempAVG < 3 {
 						hill.End = r.Sections[j-1]
-						//log.Printf("End Location of Hill: %v,%v grade: %v\n", hill.End.End.Latitude,
-						//	hill.End.End.Longitude, r.Sections[i].Grade)
 						i = j
 						if hill.Category() != "None" {
 							r.Hills = append(r.Hills, hill)
-							// fmt.Printf("Hill found, length: %4d m with avg Grade : %.2f%%\n", len(hill.Sections)*300, hill.AverageGrade)
 						}
 						break
 					}
 				} else {
 					hill.Sections = append(hill.Sections, r.Sections[j])
 					hill.AverageGrade = (hill.AverageGrade + r.Sections[j].Grade) / 2
-					// fmt.Printf("Hill Section Added: %4d grade: %.2f%%\n", j, r.Sections[j].Grade)
 				}
 			}
 
 		}
 	}
-	// log.Printf("# Hills found: %v", len(r.Hills))
-	// var start, end Location
-	// var hill Hill
 }
