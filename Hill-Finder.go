8b24a3b417bf86bec32570d1b0c033cc6acbb558
Stephen Russell <stephenjohnrussell@me.com>
2018-05-28 14:43:53 +0100

    Adding to hill algorithm

--- Hill-Finder.go
+++ Hill-Finder.go
@@ -2,17 +2,40 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/daflad/Hill-Finder/route"
 )
 
 func main() {
-	var r route.Route
-	r.Data.Open("TestData/Evening ride to Pen-y-pass and back.gpx")
-	r.GetMetrics()
-	r.FindClimbs()
-	fmt.Println(r.String())
-	for _, hill := range r.Hills {
-		fmt.Println(hill.String())
+	if len(os.Args) > 1 {
+		pathToGPX := os.Args[1]
+		files, err := ioutil.ReadDir(pathToGPX)
+		if err != nil {
+			log.Println(err)
+		} else {
+			for _, file := range files {
+				if strings.Contains(file.Name(), "Ride") {
+					fp := pathToGPX + "/" + file.Name()
+					if pathToGPX[len(pathToGPX)-1] == '/' {
+						fp = pathToGPX + file.Name()
+					}
+					var r route.Route
+					r.Data.Open(fp)
+					r.GetMetrics()
+					r.FindClimbs()
+					if len(r.Hills) > 0 {
+						fmt.Println(r.String())
+						for _, hill := range r.Hills {
+							fmt.Println(hill.String())
+						}
+					}
+					break
+				}
+			}
+		}
 	}
 }
